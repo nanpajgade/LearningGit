761e67bbda7160a302cda65ff12b35202efd1a56
nanpajgade <pajgadenandini2403@gmail.com>
2024-01-20 20:00:01 +0530

    new file diff.py added

---
+++ diff.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+
+diff_result = num1 - num2
+
+print(f"The difference between {num1} and {num2} is: {diff_result}")
+
